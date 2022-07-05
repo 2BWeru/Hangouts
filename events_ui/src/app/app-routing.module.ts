9b9b6066e6628600b65ac340b5c12349fd9a9849
Dean Kago <martokhago@gmail.com>
2022-07-05 14:07:18 +0300

    created events front end UI

--- events_ui/src/app/app-routing.module.ts
+++ events_ui/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'events', component: EventsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingcomponents = [ HomeComponent, EventsComponent];
--- events_ui/src/app/app.module.ts
+++ events_ui/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingcomponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+// import { HomeComponent } from './home/home.component';
+// import { EventsComponent } from './events/events.component';
+// import { RouterModule, Routes } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    routingcomponents,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // RouterModule.forRoot([
+    //   {path: '', component: HomeComponent},
+    //   {path: 'events', component: EventsComponent},
+    // ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
