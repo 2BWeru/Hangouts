186ea6a60f1988dd4a96adbd1ae3faa4ebc8d6d6
celine <chepceline25@gmail.com>
2022-07-12 15:21:27 +0300

    add comment

--- events_ui/src/app/app.module.ts
+++ events_ui/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SitesComponent } from './sites/sites.component';
+import { RouterModule } from '@angular/router';
+import { CommentComponent } from './comment/comment.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SitesComponent
+    SitesComponent,
+    CommentComponent,
+    
+    
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'location/',component:SitesComponent
+        
+      },
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
